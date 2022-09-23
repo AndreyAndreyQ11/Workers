3943634f5292fd9a28d57210fd881dcae9f6bc0a
AndreyAndreyQ11 <aayy6491@gmail.com>
2022-09-23 11:25:17 +0300

    test1

---
+++ js/script.js
@@ -0,0 +1,39 @@
+
+
+
+
+foo1()
+function foo1() {
+
+  let number1;
+  let number2;
+  let number3;
+
+  for (let i = 0; i < 10; i++) {
+    if (i === 9) {
+      number1 = 'test1';
+    }
+
+  };
+
+
+  for (let i = 0; i < 10; i++) {
+    console.log('test2');
+    setInterval(1000)
+  };
+
+  for (let i = 0; i < 10; i++) {
+    console.log('test3');
+    setInterval(1500)
+  };
+
+
+
+}
+
+
+
+
+
+
+
